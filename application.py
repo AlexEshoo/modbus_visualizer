99a4c37c64a615fafcb5df43135b302f0b579cea
Alex Eshoo <AlexEshoo@gmail.com>
2018-04-23 23:11:48 -0400

    Reorganized using `moveToThread` and signal slot model.

--- application.py
+++ application.py
@@ -1,55 +1,93 @@
 import sys
 import time
-import threading
 from pymodbus.client.sync import ModbusTcpClient
 from modbus_visualizer_gui import Ui_MainWindow
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
-from PyQt5.QtCore import QThread, pyqtSignal, pyqtSlot
+from PyQt5.QtCore import QObject, QThread, pyqtSignal, pyqtSlot
 
 
-class PollingThread(QThread):
+class ModbusWorker(QObject):
+    """
+    Possibe for use with movetoThread.
+    """
 
-    finished = pyqtSignal(list)
+    data_available = pyqtSignal(list)
+    new_connection_available = pyqtSignal()
+    console_message_available = pyqtSignal(str)
 
-    def __init__(self, modbus, start, length, register_type):
+    def __init__(self):
         super().__init__()
 
-        self.client = modbus
-        self.start_reg = start
-        self.length = length
-        self.register_type = register_type
+        self.client = None
+        self.busy = False  # todo: maybe a decorator for this would be nice?
 
-        self.start()
+    def isBusy(self):
+        return self.busy
 
-    def run(self):
+    @pyqtSlot(dict)
+    def configure_client(self, settings):
+        self.busy = True
+        if settings["network_type"] is "tcp":
+            host = settings["host"]
+            port = settings["port"]
+            self.client = ModbusTcpClient(host, port)
 
-        if self.register_type == "Coils":
-            rr = self.client.read_coils(self.start_reg, self.length)
-            data = rr.bits[:self.length]
+        elif settings["network_type"] is "rtu":
+            ...
+        else:
+            self.console_message_available.emit("Unknown Network Type")
 
-        elif self.register_type == "Discrete Inputs":
-            rr = self.client.read_discrete_inputs(self.start_reg, self.length)
-            data = rr.bits[:self.length]
+        connected = self.client.connect()
+        if connected:
+            self.new_connection_available.emit()
+            self.console_message_available.emit("Connection Successful")
+        else:
+            self.console_message_available.emit("Connection Failed")
 
-        elif self.register_type == "Input Registers":
-            rr = self.client.read_input_registers(self.start_reg, self.length)
+        self.busy = False
+
+    @pyqtSlot(int, int, str)
+    def poll(self, start_reg, length, register_type):
+        data = self.get_modbus_data(start_reg, length, register_type)
+        self.data_available.emit(data)
+
+    def get_modbus_data(self, start_reg, length, register_type):
+        if register_type == "Coils":
+            rr = self.client.read_coils(start_reg, length)
+            data = rr.bits[:length]
+
+        elif register_type == "Discrete Inputs":
+            rr = self.client.read_discrete_inputs(start_reg, length)
+            data = rr.bits[:length]
+
+        elif register_type == "Input Registers":
+            rr = self.client.read_input_registers(start_reg, length)
             data = rr.registers
 
-        elif self.register_type == "Holding Registers":
-            rr = self.client.read_holding_registers(self.start_reg, self.length)
+        elif register_type == "Holding Registers":
+            rr = self.client.read_holding_registers(start_reg, length)
             data = rr.registers
 
         else:
-            self.write_console("Unknown Register Type.")
+            self.console_message_available.emit("Register Type Unknown")
             data = []
 
-        self.finished.emit(data)
+        return data
 
 
+class VisualizerApp(Ui_MainWindow, QObject):
 
-class VisualizerApp(Ui_MainWindow):
+    modbus_settings_changed = pyqtSignal(dict)
+    polling_settings_available = pyqtSignal(int, int, str)
+
     def __init__(self, main_window):
+        super().__init__()
         self.setupUi(main_window)
+
+        self.worker_thread = QThread()
+        self.worker = ModbusWorker()
+        self.worker.moveToThread(self.worker_thread)
+        self.worker_thread.start()
 
         self.connect_slots()
         self.init_poll_table()
@@ -57,13 +95,15 @@ class VisualizerApp(Ui_MainWindow):
 
         self.configure_modbus_client()
 
-        self.connected = False
-        self.polling = False
-        self.poll_thread = None
-
     def connect_slots(self):
         self.singlePollPushButton.clicked.connect(self.single_poll)
         self.startRegisterSpinBox.valueChanged.connect(self.update_poll_table_column_headers)
+
+        self.modbus_settings_changed.connect(self.worker.configure_client)
+        self.worker.console_message_available.connect(self.write_console)
+        self.worker.new_connection_available.connect(self.poll_modbus_data)
+        self.polling_settings_available.connect(self.worker.poll)
+        self.worker.data_available.connect(self.write_poll_table)
 
     def init_poll_table(self):
         """
@@ -94,7 +134,7 @@ class VisualizerApp(Ui_MainWindow):
             for i in range(num_rows):
                 self.pollTable.item(i, j).setText("")
 
-    # @pyqtSlot(list)  # Works without this. Not sure why this breaks it. Fails to Connect the method for some reason...
+    @pyqtSlot(list)  # Works without this. Not sure why this breaks it. Fails to Connect the method for some reason...
     def write_poll_table(self, data):
         self.clear_poll_table()
         num_rows = self.pollTable.rowCount()
@@ -107,74 +147,41 @@ class VisualizerApp(Ui_MainWindow):
                 cur_col += 1
 
     def configure_modbus_client(self):
-        tcp_mode = self.tcpRadioButton.isChecked()
-        rtu_mode = self.rtuRadioButton.isChecked()
+        if not self.worker.isBusy():
+            tcp_mode = self.tcpRadioButton.isChecked()
+            rtu_mode = self.rtuRadioButton.isChecked()
 
-        if rtu_mode:
-            pass
-        elif tcp_mode:
-            host = self.tcpHostLineEdit.text()
-            port = self.tcpPortLineEdit.text()
-            self.client = ModbusTcpClient(host, port)
+            settings = {}
+            if rtu_mode:
+                pass
+            elif tcp_mode:
+                settings["network_type"] = "tcp"
+                settings["host"] = self.tcpHostLineEdit.text()
+                settings["port"] = self.tcpPortLineEdit.text()
 
-        self.connected = self.client.connect()  # TODO: This can block for up to the timeout of the modbus client.
-
-        if not self.connected:
-            self.write_console("Could not connect.")
-
+            self.modbus_settings_changed.emit(settings)
         else:
-            self.write_console("Connection Successful.")
+            self.write_console("Busy...")
 
     def single_poll(self):
         self.configure_modbus_client()
-        data = []
-        if self.connected:
-            start = self.startRegisterSpinBox.value()
-            length = self.numberOfRegistersSpinBox.value()
-            register_type = self.registerTypeComboBox.currentText()
-            self.poll_thread = PollingThread(self.client, start, length, register_type)
+        # Not an ideal control sequence.
+        # This starts a sequence of events that are emitted to call subsequent functions.
+        # This can be structured better. Or at least named better.
 
-            self.poll_thread.finished.connect(self.write_poll_table)
-
-
-        if data:
-            self.write_console("Poll Successful")
-
+    @pyqtSlot()
     def poll_modbus_data(self):
-        self.configure_modbus_client()
-
-        if not self.connected:
-            return []
 
         start = self.startRegisterSpinBox.value()
         length = self.numberOfRegistersSpinBox.value()
         register_type = self.registerTypeComboBox.currentText()
 
-        if register_type == "Coils":
-            rr = self.client.read_coils(start, length)
-            data = rr.bits[:length]
+        self.polling_settings_available.emit(start, length, register_type)
 
-        elif register_type == "Discrete Inputs":
-            rr = self.client.read_discrete_inputs(start, length)
-            data = rr.bits[:length]
-
-        elif register_type == "Input Registers":
-            rr = self.client.read_input_registers(start, length)
-            data = rr.registers
-
-        elif register_type == "Holding Registers":
-            rr = self.client.read_holding_registers(start, length)
-            data = rr.registers
-
-        else:
-            self.write_console("Unknown Register Type.")
-            data = []
-
-        return data
-
+    @pyqtSlot(str)
     def write_console(self, msg, *args, **kwargs):
         self.consoleLineEdit.setText("")
-        time.sleep(0.05)
+        time.sleep(0.05)  # Seems to be doing nothing now...
         self.consoleLineEdit.setText(msg)
 
     def exit(self):
