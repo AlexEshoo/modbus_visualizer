e0f0149da3790d486344c955cf453363106de4f4
Alex Eshoo <AlexEshoo@gmail.com>
2018-05-03 23:27:20 -0400

    added base formatting and tested to hell

--- visualizer/utils.py
+++ visualizer/utils.py
@@ -1,10 +1,20 @@
 import struct
-from pymodbus.payload import BinaryPayloadDecoder, Endian
+
+def digit_to_char(digit):
+    if digit < 10:
+        return str(digit)
+    return chr(ord('a') + digit - 10)
+
+def str_base(number,base):
+    if number < 0:
+        return '-' + str_base(-number, base)
+    (d, m) = divmod(number, base)
+    if d > 0:
+        return str_base(d, base) + digit_to_char(m)
+    return digit_to_char(m)
 
 def format_data(data, dtype:str, byte_order=">", word_order=">", base=10):
     raw_byte_str = struct.pack(byte_order + 'H'*len(data), *data)
-    print("raw", raw_byte_str)
-
     size = struct.calcsize(dtype)
     stub = len(raw_byte_str) % size
 
@@ -13,44 +23,26 @@ def format_data(data, dtype:str, byte_order=">", word_order=">", base=10):
     else:
         byte_str = raw_byte_str[:]
 
-    print("original byte_str", byte_str)
-
-    if word_order == "<":
-        byte_str = b"".join([ byte_str[i:i+2][::-1] for i in range(0,len(byte_str),2) ])
-
     fmt_len = len(byte_str) // size
-    result = struct.unpack(word_order + dtype*fmt_len, byte_str)
+    if size == 4:
+        if (word_order == "<"):
+            byte_str = b"".join([ byte_str[i:i+2][::-1] for i in range(0,len(byte_str),2) ])
 
-    print("final byte_Str", byte_str)
+        result = struct.unpack(word_order + dtype * fmt_len, byte_str)
 
-    return result
+    else:
+        result = struct.unpack(">" + dtype*fmt_len, byte_str)
+
+    num_base_prefixes = {2: "0b",
+                         8: "0o",
+                         16: "0x"}
+
+    prefix = num_base_prefixes.get(base, "")
+    if prefix:
+        return [ prefix + str_base(i, base).rjust(size*8, '0') for i in result ]
+    else:
+        return [ str_base(i, base) for i in result ]
 
 if __name__ == '__main__':
-
-    # Verification for -3.001957
-    d = struct.pack('B'*4, *[0xC0, 0x40, 0x20, 0x10])  # Big Endian, Big Word Order
-    print("DATA", d)
-    print('\n')
-
-    # BE, BW
-    BEBW = [ (d[0] << 8) + d[1], (d[2] << 8) + d[3] ]
-    result = format_data(BEBW, 'f', byte_order=">", word_order='>')
-    print("BEBW Result:", result)
-    print('\n')
-
-    # LE, BW
-    LEBW = [ (d[1] << 8) + d[0], (d[3] << 8) + d[2] ]
-    result = format_data(LEBW, 'f', byte_order='<', word_order='>')
-    print("LEBW Result:", result)
-    print('\n')
-
-    # BE, LW
-    BELW = [(d[2] << 8) + d[3], (d[0] << 8) + d[1]]
-    result = format_data(BELW, 'f', byte_order='>', word_order='<')
-    print("BELW Result:", result)
-    print('\n')
-
-    # LE, LW
-    LELW = [(d[3] << 8) + d[2], (d[1] << 8) + d[0]]
-    result = format_data(LELW, 'f', byte_order='<', word_order='<')
-    print("LELW Result:", result)
+    x = str_base(20, 2).rjust(8,'0')
+    print(x)
