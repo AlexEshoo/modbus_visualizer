d470a9d2cec8b1d8123343f8c976b0c1437b3793
Alex Eshoo <AlexEshoo@gmail.com>
2018-05-06 10:41:33 -0400

    added function to get available serial ports

--- visualizer/utils.py
+++ visualizer/utils.py
@@ -1,4 +1,7 @@
+import glob
+import serial
 import struct
+import sys
 from math import log, floor
 
 def digit_to_char(digit):
@@ -66,6 +69,36 @@ def format_data(data, dtype:str, byte_order=">", word_order=">", base=10):
 
     return formatted
 
+
+def serial_ports():
+    """
+        Thanks to: https://stackoverflow.com/questions/12090503/listing-available-com-ports-with-python
+        Lists available serial port names
+
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
 if __name__ == '__main__':
-    x = str_base(20, 2).rjust(8,'0')
-    print(x)
+    print(serial_ports())
